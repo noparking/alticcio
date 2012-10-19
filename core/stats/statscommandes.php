8c2c14ce2cdfc029b82f8fc90300b01c8d22e863
ameunier <ameunier@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2012-10-19 15:47:31 +0000

    import des premières classes de stats
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2629 4f6a263e-5ccd-11de-98f9-81b68c04c31c

---
+++ core/stats/statscommandes.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ * Classe PHP qui génére des statistiques sur les contacts
+ */
+
+
+class StatsCommandes {
+	
+	public function __construct($sql = null, $dico) {
+		$this->sql = $sql;
+		$this->dico = $dico;
+	}
+	
+	public function nombre_commandes_par_mois() {
+		$q = "SELECT DATE_FORMAT(FROM_UNIXTIME(c.date_commande), '%Y') AS year, DATE_FORMAT(FROM_UNIXTIME(c.date_commande), '%m') AS month, COUNT(*) AS total
+				FROM dt_commandes as c
+				WHERE (c.paiement != 'refuse'  OR c.paiement != 'annule') 
+				GROUP BY year, month ";
+		$rs = $this->sql->query($q);
+		$tab = array();
+		while ($row = $this->sql->fetch($rs)) {
+			$tab[] = $row;
+		};
+		return $tab;
+	}
+	
+	public function chiffre_affaires_par_mois() {
+		$q = "SELECT DATE_FORMAT(FROM_UNIXTIME(c.date_commande), '%Y') AS annee, DATE_FORMAT(FROM_UNIXTIME(c.date_commande), '%m') AS mois, SUM(c.montant) AS total
+				FROM dt_commandes as c
+				WHERE (c.paiement != 'refuse'  OR c.paiement != 'annule') 
+				GROUP BY annee, mois";
+		$rs = $this->sql->query($q);
+		$tab = array();
+		while ($row = $this->sql->fetch($rs)) {
+			$tab[] = $row;
+		};
+		return $tab;
+	}
+	
+	public function afficher_tableau($valeurs) {
+		$html = '<table>';
+		foreach($valeurs as $k => $v) {
+			
+		}
+		
+		
+		
+		
+		foreach($valeurs as $k => $v) {
+			$details_date = explode("-",$v['month']);
+			if ($prev_annee != $details_date[0]) {
+				if ($n > 0) {
+					$html .= '</tr>';
+				}
+				$html .= '<tr>';
+				$html .= '<td>'.$details_date[0].' '.$v['month'].'</td>';
+				foreach($mois as $m => $month) {
+					$html .= '<td>';
+					if (isset($details_date[1]) AND $m == $details_date[1]) {
+						$html .= $v['total'];
+					}
+					$html .= '</td>';
+				}
+			}
+			$prev_annee = $details_date[0];
+			$n++;
+		}
+		$html .= '</tr>';
+		$html .= '</table>';
+		return $html;
+	}
+}
+?>
---
+++ core/stats/statsurl.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Classe PHP qui génére des statistiques sur les contacts
+ */
+
+
+class StatsUrl {
+	
+	public function __construct($sql = null, $dico, $annee) {
+		$this->sql = $sql;
+		$this->dico = $dico;
+		$this->annee = $annee;
+	}
+	
+	private function timestamp_debut() {
+		return mktime(0, 0, 0, 1, 1, $this->annee);
+	}
+	
+	private function timestamp_fin() {
+		return mktime(0, 0, 0, 1, 1, ($this->annee + 1));
+	}
+	
+	private function lister_resultats() {
+		$debut = $this->timestamp_debut();
+		$fin = $this->timestamp_fin();
+		$q = 'SELECT DATE_FORMAT(FROM_UNIXTIME(s.date_requete), "%m") AS month, COUNT(*) AS total
+				FROM shorturl_log as s
+				WHERE s.date_requete > '.$debut.' AND s.date_requete < '.$fin.'
+				GROUP BY month ';
+		$rs = $this->sql->query($q);
+		$row = $this->sql->fetch($rs);
+		return $row;
+	}
+
+	public function tableau_resultats($months) {
+		$resultats = $this->lister_resultats();
+		$html = '<table id="" name="" summary="">';
+		$html .= '<thead>';
+		$html .= '<tr>';
+		foreach($months as $key => $value) {
+			$html .= '<td>'.$value.'</td>';
+		}
+		$html .= '</tr>';
+		$html .= '</thead>';
+		$html .= '<tbody>';
+		$html .= '<tr>';
+		foreach($resultats as $k => $v) {
+			$html .= '<td>'.$value.'</td>';
+		}
+		$html .= '</tr>';
+		$html .= '</tbody>';
+		$html .= '</table>';
+	}
+}
+?>
