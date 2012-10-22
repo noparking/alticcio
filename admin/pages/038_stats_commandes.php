ef8cf16e32bd13a0c36f9ad0bfc88f25f244e17b
ameunier <ameunier@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2012-10-22 16:26:24 +0000

    ajout de stats de vente
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2635 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- admin/pages/038_stats_commandes.php
+++ admin/pages/038_stats_commandes.php
@@ -19,12 +19,19 @@ $stats = new StatsCommandes($sql, $dico);
 
 $titre_page = $dico->t("StatsCommandes");
 
-
+// Nombre de commandes total
 $nbre_commandes_mois = $stats->nombre_commandes_par_mois();
 $tableau_nbre_commandes_mois = $stats->afficher_tableau($nbre_commandes_mois);
 
+// CA total
+$ventes_totales_mois = $stats->chiffre_affaires_par_annee_mois();
+$tableau_ventes_mois = $stats->afficher_tableau($ventes_totales_mois);
+
 $main = <<<HTML
+<h3>Nbre de commandes</h3>
 $tableau_nbre_commandes_mois
+<h3>Ventes</h3>
+$tableau_ventes_mois
 HTML;
 
 $right = "";
