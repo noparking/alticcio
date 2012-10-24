94421c097025c9bbbc761b895ab43f076b28da4c
ameunier <ameunier@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2012-10-24 09:52:33 +0000

    correction sur les stats shorturl
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2638 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- admin/pages/037_stats_shorturl.php
+++ admin/pages/037_stats_shorturl.php
@@ -16,58 +16,14 @@ $menu->current('main/stats/shorturl');
  * On initialise la classe StatsContacts 
  */
 $sql = new Mysql($config->db());
-
-$annees = array();
-for($i=2012; $i<=date('Y'); $i++) {
-	$annees[$i] = $i;
-}
-
-$months = array(	"01" => $dico->t('MoisJanvier'),
-				"02" => $dico->t('MoisFevrier'),
-				"03" => $dico->t('MoisMars'),
-				"04" => $dico->t('MoisAvril'),
-				"05" => $dico->t('MoisMai'),
-				"06" => $dico->t('MoisJuin'),
-				"07" => $dico->t('MoisJuillet'),
-				"08" => $dico->t('MoisAout'),
-				"09" => $dico->t('MoisSeptembre'),
-				"10" => $dico->t('MoisOctobre'),
-				"11" => $dico->t('MoisNovembre'),
-				"12" => $dico->t('MoisDecembre') );
-
-$form = new Form(array(
-	'id' => "form-search-shorturl",
-	'class' => "form-search-shorturl",
-	'required' => array(),
-));
-
-
-
-if ($form->is_submitted() and $form->validate()) {
-	$datas = $form->values();
-	$stats = new StatsUrl($sql, $dico, $datas['annee']);
-	print_r($stats->lister_resultats());
-}
-else {
-	$datas = array();
-	$datas['annee'] = date("Y");
-	$table_url = "";
-}
-
-
+$stats = new StatsUrl($sql, $dico);
 
 $titre_page = $dico->t("StatsURL");
+$nbre_short_url = $stats->lister_resultats();
+$tableau_short_url = $stats->afficher_tableau($nbre_short_url);
 
 $main = <<<HTML
-{$form->form_start()}
-{$form->fieldset_start($dico->t(""))}
-{$form->select(array('name' => "annee", 'label' => $dico->t('SelectAnnee'), 'options' => $annees, 'value' => $datas['annee'] ))}
-{$form->input(array('type'=>'submit', 'name'=>'search', 'value'=> $dico->t('Envoyer') )) }</p>
-{$form->fieldset_end()}
-{$form->form_end()}
-
-<h3>{$dico->t('NbreUrlMois')}</h3>
-<br/>
+$tableau_short_url
 HTML;
 
 $right = "";
