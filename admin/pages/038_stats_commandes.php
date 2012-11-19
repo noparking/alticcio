042eb20be49c64afd311676368b139579c75f169
ameunier <ameunier@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2012-11-19 08:33:08 +0000

    Import stats panier_moyen_par_annee_mois
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2683 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- admin/pages/038_stats_commandes.php
+++ admin/pages/038_stats_commandes.php
@@ -25,13 +25,19 @@ $tableau_nbre_commandes_mois = $stats->afficher_tableau($nbre_commandes_mois);
 
 // CA total
 $ventes_totales_mois = $stats->chiffre_affaires_par_annee_mois();
-$tableau_ventes_mois = $stats->afficher_tableau($ventes_totales_mois);
+$tableau_ventes_mois = $stats->afficher_tableau($ventes_totales_mois, "montant");
+
+// Panier moyen
+$panier_moyen_mois = $stats->panier_moyen_par_annee_mois();
+$tableau_panier_mois = $stats->afficher_tableau($panier_moyen_mois, "montant");
 
 $main = <<<HTML
 <h3>Nbre de commandes</h3>
 $tableau_nbre_commandes_mois
 <h3>Ventes</h3>
 $tableau_ventes_mois
+<h3>Panier moyen</h3>
+$tableau_panier_mois
 HTML;
 
 $right = "";
