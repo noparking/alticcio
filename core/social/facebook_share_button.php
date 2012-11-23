0487a3d27cd1bb9c4fb1953ccd5978a1114d5a4d
geoffrey.bauduin <geoffrey.bauduin@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2012-11-23 13:12:35 +0000

    Modification sur le bouton Share (facebook)
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2717 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- core/social/facebook_share_button.php
+++ core/social/facebook_share_button.php
@@ -7,11 +7,28 @@ class Facebook_Share_Button {
 	}
 	
 	function generer_bouton() {
+		global $config, $page;
+		$js = <<<Javascript
+$(document).ready(function() {
+	$("#fb-partage").click(function() {
+		FB.init({appId: "486457814727883", status: true, cookie: true});
+		var obj = {
+			method: 'feed',
+			redirect_uri: facebook_wall_url,
+			link: 'https://developers.facebook.com/docs/reference/dialogs/',
+			picture: '{$config->media("logo.png")}',
+			name: 'Mon tapis de jeu Aberlaas',
+			caption: 'Aberlaas',
+			description: "Comment trouvez-vous le tapis que j'ai choisi ?",
+		};
+		function callback(reponse) { }
+		FB.ui(obj, callback);
+	});
+});
+Javascript;
+		$page->post_javascript[] = $js;
 		return <<<HTML
-<a name="fb_share" id="fb-partage" type="button" share_url="{$this->href}" href="http://www.facebook.com/sharer.php">
-	Partager
-</a>
-<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+<img src="{$config->media("facebook.png")}" id="fb-partage" style="cursor:pointer;"/>
 HTML;
 	}
 }
