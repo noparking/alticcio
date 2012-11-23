db8d19786f5f57aa753e7431e345e6ce4264cca1
geoffrey.bauduin <geoffrey.bauduin@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2012-11-23 13:21:14 +0000

    Correction d'une faute de frappe.
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2719 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- core/social/facebook_share_button.php
+++ core/social/facebook_share_button.php
@@ -1,6 +1,11 @@
 <?php
 class Facebook_Share_Button {
 	public $href;
+	public $redirect_uri;
+	public $picture;
+	public $name;
+	public $caption;
+	public $description;
 	
 	function __construct(array $data) {
 		$this->href = $data['href'];
@@ -16,7 +21,7 @@ $(document).ready(function() {
 			method: 'feed',
 			redirect_uri: facebook_wall_url,
 			link: 'https://developers.facebook.com/docs/reference/dialogs/',
-			picture: '{$config->media("site_url")}{$config->media("logo.png")}',
+			picture: '{$config->get("site_url")}{$config->media("logo.png")}',
 			name: 'Mon tapis de jeu Aberlaas',
 			caption: 'Aberlaas',
 			description: "Comment trouvez-vous le tapis que j'ai choisi ?",
