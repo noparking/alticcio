c32f0fa6f1d4eae8145e668e149895c0ddd4c3be
geoffrey.bauduin <geoffrey.bauduin@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2012-11-30 14:09:58 +0000

    Application des changements de facebook à Pinterest
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2738 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- core/social/pinterest_pin_it_button.php
+++ core/social/pinterest_pin_it_button.php
@@ -4,14 +4,19 @@ class Pinterest_Pin_It_Button {
 	public $media;
 	public $description;
 	public $id;
+	public $js;
 	
 	function __construct(array $data) {
 		$this->url = $data['url'];
 		$this->media = $data['media'];
 		$this->description = $data['description'];
 		$this->id = isset($data['id']) ? $data['id'] : uniqid();
+		$this->js = isset($data['js']) ? $data['js'] : "send_to_pinterest()";
 	}
 	
+	function getId() {
+		return "pinterest-{$this->id}";
+	}
 	
 	function generer_bouton() {
 		global $page;
@@ -19,14 +24,23 @@ class Pinterest_Pin_It_Button {
 		if (!in_array("http://assets.pinterest.com/js/pinit.js", $page->javascript)) {
 			$page->javascript[] = "http://assets.pinterest.com/js/pinit.js";
 		}
-		$link = "http://pinterest.com/pin/create/button/?";
-		$link .= "url=".urlencode($this->url);
-		$link .= "&media=".urlencode($this->media);
-		$link .= "&description=".urlencode($this->description);
-		return <<<HTML
-<a href="{$link}" class="pin-it-button" count-layout="none" id="pinterest_{$data['id']}">
-	<img border="0" src="http://assets.pinterest.com/images/PinExt.png" title="Pin It" />
-</a>
+		$js = <<<Javascript
+function send_to_pinterest() {
+	var url = encodeURI({$this->url});
+	var media = encodeURI({$this->media});
+	var description = encodeURI("{$this->description}");
+	var newWindowUrl = "http://pinterest.com/pin/create/button/?url="+url+"&media="+media+"&description="+description;
+	window.open(newWindowUrl,'name','height=350,width=600');
+}
+$(document).ready(function() {
+	$("#{$this->getId()}").click(function() {
+		{$this->js}
+	});
+});
+Javascript;
+	$page->post_javascript[] = $js;
+	return <<<HTML
+<img border="0" src="http://assets.pinterest.com/images/PinExt.png" id="{$this->getId()}" style="cursor: pointer;" title="Pin It" />
 HTML;
 	}
 }
