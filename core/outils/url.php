e78a19236ba04830f0b15d9e1293f9ba33e73d7e
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2013-03-20 11:57:55 +0000

    Ajout d'une méthode always_redirect et d'une méthode canonical_redirect au niveau des urls
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2801 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- core/outils/url.php
+++ core/outils/url.php
@@ -3,14 +3,22 @@
 abstract class Url {
 	
 	private $base = "";
-	private $elements;
+	private $elements = array();
+	private $elements_with_trailing_slashes = array();
 	
 	public function set_base($base) {
 		$this->base = $base;
 	}
 	
 	public function elements() {
-		$this->elements = func_get_args();
+		$elements = func_get_args();
+		foreach ($elements as $element) {
+			if (substr($element, -1) == "/") {
+				$element = substr($element, 0, -1);
+				$this->elements_with_trailing_slashes[] = $element;
+			}
+			$this->elements[] = $element;
+		}
 	}
 	
 	public function get($element) {
@@ -36,12 +44,17 @@ abstract class Url {
 		$args = func_get_args();
 		$elements = call_user_func_array(array($this, 'build'), $args);
 		$elements_list = array();
+		$trailing_slash = "";
 		foreach ($this->elements as $element) {
 			if ($elements[$element]) {
+				$trailing_slash = "";
+				if (in_array($element, $this->elements_with_trailing_slashes)) {
+					$trailing_slash = "/";
+				}
 				$elements_list[] = $elements[$element];
 			}
 		}
-		return $this->base.implode("/", $elements_list);
+		return $this->base.implode("/", $elements_list).$trailing_slash;
 	}
 	
 	public function redirect() {
@@ -57,5 +70,18 @@ abstract class Url {
 		header("Location: ".$url.$query_string);
 		exit;
 	}
-	
+
+	public function always_redirect() {
+		header('HTTP/1.1 301 Moved Permanently', false, 301);
+		$args = func_get_args();
+		call_user_func_array(array($this, 'redirect'), $args);
+	}
+
+	public function canonical_redirect() {
+		$args = func_get_args();
+		$url = call_user_func_array(array($this, 'make'), $args);
+		if ($url != $_SERVER['REQUEST_URI']) {
+			call_user_func_array(array($this, 'always_redirect'), $args);
+		}
+	}
 }
