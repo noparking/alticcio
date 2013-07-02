351bb4fb7e11926f84db0917a4c9bdf622306bd7
ameunier <ameunier@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2013-07-02 09:18:20 +0000

    réécriture de la requête et des règles de calcul
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2864 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- admin/pages/031_stats_satisfaction.php
+++ admin/pages/031_stats_satisfaction.php
@@ -16,6 +16,12 @@ $menu->current('main/stats/satisfaction');
  */
 $sql = new Mysql($config->db());
 
+$form = new Form(array(
+	'id' => "form-search-satisfaction",
+	'class' => "form-search-satisfaction",
+	'required' => array(),
+));
+
 $months = array(	"01" => $dico->t('MoisJanvier'),
 					"02" => $dico->t('MoisFevrier'),
 					"03" => $dico->t('MoisMars'),
@@ -29,6 +35,29 @@ $months = array(	"01" => $dico->t('MoisJanvier'),
 					"11" => $dico->t('MoisNovembre'),
 					"12" => $dico->t('MoisDecembre') );
 
+
+/*
+ * Fonctions de traitement
+ */				
+function moyenne($note, $nbre, $sur) {
+	if ($note > 0) {
+		$moyenne = $note/$nbre;
+		return '<strong>'.round($moyenne,1).'</strong>/'.$sur;
+	}
+	else {
+		return "-";
+	}
+}
+function pourcentage_satisfait($nb_satisfait, $nb_total) {
+	if ($nb_total > 0) {
+		return round((($nb_satisfait*100)/$nb_total),1).'%';
+	}
+	else {
+		return "";
+	}
+}					
+
+	
 /*
  * Mesure de la satisfaction :
  * Si la réponse d'un sondage comprend que des notes de 3 et/ou 4, on le considère comme satisfait
@@ -41,36 +70,25 @@ while($row = $sql->fetch($rs)) {
 		$rs1 = $sql->query($q1);
 	}
 }					
-										
-					
-$form = new Form(array(
-	'id' => "form-search-satisfaction",
-	'class' => "form-search-satisfaction",
-	'required' => array(),
-));
-
-function moyenne($note, $nbre, $sur) {
-	if ($note > 0) {
-		$moyenne = $note/$nbre;
-		return '<strong>'.round($moyenne,1).'</strong>/'.$sur;
-	}
-	else {
-		return "-";
-	}
-}
-function pourcentage_satisfait($nb_satisfait, $nb_total) {
-	if ($nb_total > 0) {
-		return round((($nb_satisfait*100)%$nb_total),1).'%';
-	}
-	else {
-		return "";
-	}
-}
 
 
+/*
+ * Traitement des résultats après envoi du formulaire
+ */
 $html_results = "";
 if ($form->is_submitted() and $form->validate()) {
 	$datas = $form->values();
+	if ($datas['annee'] > 2010) {
+		$debut_annee = mktime(0,0,0,1,1,$datas['annee']);
+		$fin_annee = mktime(0,0,0,1,1,($datas['annee']+1));
+		$langue_form = $datas['langue'];
+	}
+	else {
+		$debut_annee = mktime(0,0,0,1,1,2010);
+		$fin_annee = mktime(0,0,0,1,1,2011);
+		$langue_form = 'fr_FR';
+	}
+
 	$html_results .= '<table>';
 	$html_results .= '<tr>';
 	$html_results .= '<th>'.$dico->t('Mois').'</th>';
@@ -85,75 +103,69 @@ if ($form->is_submitted() and $form->validate()) {
 	$html_results .= '<th>'.$dico->t('QuestionProduit').'</th>';
 	$html_results .= '<th>'.$dico->t('NoteGlobale').'</th>';
 	$html_results .= '</tr>';
-	$liste_comments = array();
-	foreach($months as $key => $month) {
-		$date_debut = mktime(0,0,0,$key,1,$datas['annee']);
-		$date_fin = mktime(23,59,59,$key,31,$datas['annee']);
-		$q = "SELECT * FROM dt_sondage_satisfaction WHERE date_reponse >= ".$date_debut." AND date_reponse <= ".$date_fin." AND langue = '".$datas['langue']."' ";
-		$rs = $sql->query($q);
-		$q1 = 0;
-		$q2 = 0;
-		$q3 = 0;
-		$q4 = 0;
-		$q5 = 0;
-		$q6 = 0;
-		$q7 = 0;
-		$note = 0;
-		$satisfait = 0;
-		$i = 0;
-		while ($row = $sql->fetch($rs)) {
-			$q1 = $q1 + $row['q1'];
-			$q2 = $q2 + $row['q2'];
-			$q3 = $q3 + $row['q3'];
-			$q4 = $q4 + $row['q4'];
-			$q5 = $q5 + $row['q5'];
-			$q6 = $q6 + $row['q6'];
-			$q7 = $q7 + $row['q7'];
-			$note = $note + $row['scoring'];
-			$satisfait = $satisfait + $row['satisfait'];
-			if (!empty($row['commentaires'])) {
-				$date_comment = date('d M Y', $row['date_reponse']);
-				$html_comments = <<<HTML
-<dl>
-	<dt>{$date_comment} - {$dico->t('Commande')} {$row['num_cde']}</dt>
-	<dd>{$row['commentaires']}</dd>
-</dl>
-HTML;
-				$liste_comments[] = $html_comments;
-			}
-			$i++;
-		}
-		$nbre_reponses = $i;
+	
+	// requête en base
+	$q = "SELECT DATE_FORMAT(FROM_UNIXTIME(s.date_reponse), '%Y') AS annee,
+				DATE_FORMAT(FROM_UNIXTIME(s.date_reponse), '%m') AS mois, 
+				COUNT(*) AS total, 
+				SUM(satisfait) AS satisfait, 
+				SUM(q1) AS q1,
+				SUM(q2) AS q2,
+				SUM(q3) AS q3,
+				SUM(q4) AS q4,
+				SUM(q5) AS q5,
+				SUM(q6) AS q6,
+				SUM(q7) AS q7,
+				SUM(scoring) AS scoring
+			FROM dt_sondage_satisfaction as s
+			WHERE date_reponse > ".$debut_annee." AND date_reponse < ".$fin_annee."
+			AND langue = '".$langue_form."' 
+			GROUP BY annee, mois
+			ORDER BY annee, mois";
+	$rs = $sql->query($q);					
+	while($row = $sql->fetch($rs)) {
 		$html_results .= '<tr>';
-		$html_results .= '<td>'.$month.'</td>';
-		$html_results .= '<td>'.$nbre_reponses.'</td>';
-		$html_results .= '<td>'.pourcentage_satisfait($satisfait, $nbre_reponses).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($q1, $nbre_reponses, 4).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($q2, $nbre_reponses, 4).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($q3, $nbre_reponses, 4).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($q7, $nbre_reponses, 4).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($q4, $nbre_reponses, 4).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($q5, $nbre_reponses, 4).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($q6, $nbre_reponses, 4).'</td>';
-		$html_results .= '<td style="text-align:center;">'.moyenne($note, $nbre_reponses, 28).'</td>';
+		$html_results .= '<td>'.$row['mois'].'</td>';
+		$html_results .= '<td>'.$row['total'].'</td>';
+		$html_results .= '<td>'.pourcentage_satisfait($row['satisfait'], $row['total']).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['q1'], $row['total'], 4).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['q2'], $row['total'], 4).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['q3'], $row['total'], 4).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['q7'], $row['total'], 4).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['q4'], $row['total'], 4).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['q5'], $row['total'], 4).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['q6'], $row['total'], 4).'</td>';
+		$html_results .= '<td style="text-align:center;">'.moyenne($row['scoring'], $row['total'], 28).'</td>';
 		$html_results .= '</tr>';
 	}
 	$html_results .= '<table>';
+	
+	$q1 = "SELECT commentaires, date_reponse, num_cde FROM dt_sondage_satisfaction WHERE commentaires != '' AND date_reponse > ".$debut_annee." AND date_reponse < ".$fin_annee;
+	$rs1 = $sql->query($q1);
+	$html_results .= '<div class="commentaires_sondages">';
+	while($row1 = $sql->fetch($rs1)) {
+		$date_comment = date('d M Y', $row1['date_reponse']);
+		$html_results .= '<dl>';
+		$html_results .= '<dt>'.$date_comment.' - '.$dico->t('Commande').' '.$row1['num_cde'].'</dt>';
+		$html_results .= '<dd>'.$row1['commentaires'].'</dd>';
+		$html_results .= '</dl>';
+	}
+	$html_results .= '</div>';
 }
 else {
-	$datas = array();
+	$datas['annee'] = "";
+	$datas['langue'] = "";
 }
-
-$annees = array();
-for($i=2011; $i<=date('Y'); $i++) {
-	$annees[$i] = $i;
-}
-$langues = array( "fr_FR" => "FR", "es_ES" => "ES");
 
 
 /* 
  * Valeurs renvoyées dans le template
  */
+$annees = array();
+for($i=2011; $i<=date('Y'); $i++) {
+	$annees[$i] = $i;
+}
+$langues = array( "fr_FR" => "FR", "es_ES" => "ES");
 
 $titre_page = $dico->t("StatsSondagesSatisfaction");
 
@@ -167,16 +179,6 @@ $main = <<<HTML
 {$form->form_end()}
 HTML;
 $main .= $html_results;
-if (count($liste_comments) > 0) {
-	$nouvelle_liste = array_reverse($liste_comments);
-	$main .= '<div class="commentaires_sondages">';
-	foreach($nouvelle_liste as $comment) {
-		$main .= $comment;
-	}
-	$main .= '</div>';
-}
 
 $right = "";
-
-
 ?>
