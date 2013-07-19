94d89db119ed5698e1c038e880a44c684999ebeb
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2013-07-19 14:54:42 +0000

    Remplacement de la checkbox échantillon par un select
    git-svn-id: svn://svn.noparking.net/var/repos/alticcio@2885 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- admin/pages/011_produits.produits.php
+++ admin/pages/011_produits.produits.php
@@ -543,7 +543,7 @@ if ($action == "create" or $action == "edit") {
 {$form->select(array('name' => "produit[id_gammes]", 'label' => $dico->t('Gamme'), 'options' => $produit->gammes()))}
 {$form->select(array('name' => "produit[offre]", 'label' => $dico->t('Offre'), 'options' => $offres))}
 {$form->select(array('name' => "produit[id_recyclage]", 'label' => $dico->t('FiliereRecyclage'), 'options' => $produit->recyclage($id_langues) ))}
-{$form->input(array('name' => "produit[echantillon]", 'label' => $dico->t('EchantillonDisponible'), 'type' => "checkbox"))}
+{$form->select(array('name' => "produit[echantillon]", 'label' => $dico->t('EchantillonDisponible'), 'options' => array(1 => $dico->t('Oui'), 0 => $dico->t('Non'))))}
 {$form->input(array('name' => "produit[phrase_url_key]", 'type' => "hidden"))}
 {$form->input(array('name' => "phrases[phrase_url_key]", 'label' => $dico->t('UrlKey'), 'items' => $displayed_lang))}
 {$form->input(array('name' => "produit[phrase_avantages_produit]", 'type' => "hidden"))}
