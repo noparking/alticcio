9bca1cc1f0d73891b6bf2b741cef67d856177b97
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2013-10-21 14:23:58 +0000

    Gestion des méthodes de type POST concernant le panier
    git-svn-id: svn://svn.noparking.net/usr/local/svn/alticcio@2966 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- core/api/hooks/cart-1.0.php
+++ core/api/hooks/cart-1.0.php
@@ -8,3 +8,12 @@ function get_cart() {
 	$cart = new API_Cart($api);
 	return call_user_func_array(array($cart, $method) ,$args);
 }
+
+function post_cart() {
+	$args = func_get_args();
+	$api = array_shift($args);
+	$method = array_shift($args);
+
+	$cart = new API_Cart($api);
+	return call_user_func_array(array($cart, $method) ,$args);
+}
