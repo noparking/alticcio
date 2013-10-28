fe0fb0a6aaa45b8100ded8faa030d38e76d68609
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2013-10-28 11:47:30 +0000

    Ajout de champs pour les dates au format date dans les commandes perso
    git-svn-id: svn://svn.noparking.net/usr/local/svn/alticcio@2973 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- core/export/commande_perso.php
+++ core/export/commande_perso.php
@@ -35,26 +35,28 @@ class ExportCommandePerso extends AbstractExport {
 			'quantite',
 			'personnalisation_texte',
 			'personnalisation_fichier',
+			'time_commande',
 			'date_commande',
+			'time_export',
 			'date_export',
 		);
 
 		return $fields;
 	}
 
-	public function date_last_commande() {
+	public function time_last_commande() {
 		$q = <<<SQL
-SELECT MAX(date_commande) AS date_last_commande FROM {$this->export_table}
+SELECT MAX(time_commande) AS time_last_commande FROM {$this->export_table}
 SQL;
 		$res = $this->sql_export->query($q);
 		$row = $this->sql_export->fetch($res);
 
-		return $row['date_last_commande'];
+		return $row['time_last_commande'];
 	}
 
 # TODO Gérer les révisions
 	public function cmds_perso_a_exporter($date_export) {
-		$date_last_commande = $this->date_last_commande();
+		$time_last_commande = $this->time_last_commande();
 		$q = <<<SQL
 SELECT cp.id, cp.id_commandes, cp.id_produits, cp.id_sku, cp.ref, cp.nom, cp.quantite,
 cp.personnalisation_texte, cp.personnalisation_fichier, c.date_commande
@@ -62,8 +64,8 @@ FROM dt_commandes_produits AS cp
 INNER JOIN dt_commandes AS c ON c.id = cp.id_commandes
 WHERE (cp.personnalisation_texte <> "" OR cp.personnalisation_fichier <> "")
 SQL;
-		if ($date_last_commande) {
-			$q .= " AND c.date_commande > $date_last_commande";
+		if ($time_last_commande) {
+			$q .= " AND c.date_commande > $time_last_commande";
 		}
 		$res = $this->sql->query($q);
 		$cmds = array();
@@ -78,8 +80,10 @@ SQL;
 				'quantite' => $row['quantite'],
 				'personnalisation_texte' => $row['personnalisation_texte'],
 				'personnalisation_fichier' => $row['personnalisation_fichier'],
-				'date_commande' => $row['date_commande'],
-				'date_export' => $date_export,
+				'time_commande' => $row['date_commande'],
+				'date_commande' => date("Y-m-d", $row['date_commande']),
+				'time_export' => $date_export,
+				'date_export' => date("Y-m-d", $date_export),
 			);
 		}
 
@@ -89,8 +93,11 @@ SQL;
 	public function prepare($fields) {
 		$field_list = "";
 		foreach ($fields as $field) {
-			if (in_array($field, array("id", "id_commande", "id_produit", "id_sku", "quantite", "date_commande", "date_export"))) {
+			if (in_array($field, array("id", "id_commande", "id_produit", "id_sku", "quantite", "time_commande", "time_export"))) {
 				$field_list .= "`$field` int(11) NOT NULL,";
+			}
+			else if (in_array($field, array("date_commande", "date_export"))) {
+				$field_list .= "`$field` date NOT NULL,";
 			}
 			else {
 				$field_list .= "`$field` mediumtext NOT NULL,";
