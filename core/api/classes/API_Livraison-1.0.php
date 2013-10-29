395faee7967731976b99c5ad16c9b7894d415f01
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2013-10-29 16:34:52 +0000

    Gestion des frais de livraison via l'API
    git-svn-id: svn://svn.noparking.net/usr/local/svn/alticcio@2977 4f6a263e-5ccd-11de-98f9-81b68c04c31c

---
+++ core/api/classes/API_Livraison-1.0.php
@@ -0,0 +1,105 @@
+<?php
+
+class API_Livraison {
+	private $api;
+	private $sql;
+	private $id_langues;
+
+	public function __construct($api){
+		$this->api = $api;
+		$this->sql = $api->sql;
+	} 
+	
+	public function liste_pays($id_langues){
+		$q = <<<SQL
+SELECT DISTINCT(fp.id_pays), ph.phrase FROM dt_frais_port AS fp 
+INNER JOIN dt_pays AS p ON fp.id_pays = p.id
+INNER JOIN dt_phrases AS ph ON p.phrase_nom = ph.id AND ph.id_langues = {$id_langues} 
+WHERE fp.id_langues = {$id_langues}
+SQL;
+	
+		$liste_pays = array();
+		$res = $this->sql->query($q);
+		while ($row = $this->sql->fetch($res)){
+			$liste_pays[$row['id_pays']] = $row['phrase'];
+		}
+		
+		return $liste_pays;
+	}
+	
+	public function forfaits($id_langues, $id_boutiques = 0){
+		$q = <<<SQL
+SELECT fp.id_pays, fp.methode, fp.prix_min, fp.forfait, ph.phrase FROM dt_frais_port AS fp
+INNER JOIN dt_phrases AS ph
+ON fp.phrase_info = ph.id
+WHERE fp.id_langues = {$id_langues} AND fp.id_boutiques = $id_boutiques 
+ORDER BY fp.prix_min ASC
+SQL;
+		$forfaits = array();
+		$res = $this->sql->query($q);
+
+		while ($row = $this->sql->fetch($res)){
+			if (!isset($forfaits[$row['id_pays']])){
+				$forfaits[$row['id_pays']] = array(
+					'methode' => $row['methode'],
+					'info' => $row['phrase'],
+					'tarifs' => array(),
+				);
+			}
+			$forfaits[$row['id_pays']]['tarifs'][$row['prix_min']] = array(
+				'prix_min' => $row['prix_min'],
+				'prix_max' => 0,
+				'forfait' => $row['forfait'],
+			);
+		}
+		
+		foreach ($forfaits as $id_pays => $forfaits_pays) {
+			$j = null;
+			foreach ($forfaits_pays['tarifs'] as $i => $tarif)  {
+				if ($j !== null){
+					$forfaits[$id_pays]['tarifs'][$j]['prix_max'] = $tarif['prix_min'] - 1;
+				}
+				$j = $i;
+			}
+		}
+		
+		return $forfaits;
+	}
+
+	public function forfait ($id_langues, $montant, $id_pays, $id_boutiques = 0) {
+		$q = <<<SQL
+SELECT forfait FROM dt_frais_port WHERE prix_min <= $montant 
+AND id_langues = {$id_langues}
+AND id_pays = $id_pays
+AND id_boutiques = $id_boutiques
+ORDER BY prix_min DESC LIMIT 1
+SQL;
+		$res = $this->sql->query($q);
+		$row = $this->sql->fetch($res);
+		
+		return $row['forfait'];
+	}
+	
+	function check($id_langues, $id_boutiques = 0){
+		$data = $this->api->post;
+		$id_pays = $data['commande']['livraison_pays'];
+		$q = <<<SQL
+SELECT methode from dt_frais_port
+WHERE id_langues = {$id_langues} AND id_pays = {$id_pays} AND id_boutiques = $id_boutiques
+SQL;
+		$res = $this->sql->query($q);
+		$row = $this->sql->fetch($res);
+		if ($row === false) {
+			return false;
+		} else {
+			switch ($row['methode']) {
+				case 'HorsDomTom':
+					$debut_cp = substr($data['commande']['livraison_cp'], 0, 2);
+					return !in_array(strtoupper($debut_cp), array('97','98','2A','2B', '20'));
+					break;
+				default :
+					return true;
+			}
+		}
+	}
+}
--- core/api/hooks/cart-1.0.php
+++ core/api/hooks/cart-1.0.php
@@ -17,3 +17,21 @@ function post_cart() {
 	$cart = new API_Cart($api);
 	return call_user_func_array(array($cart, $method) ,$args);
 }
+
+function get_cart_livraison() {
+	$args = func_get_args();
+	$api = array_shift($args);
+	$method = array_shift($args);
+
+	$livraison = new API_Livraison($api);
+	return call_user_func_array(array($livraison, $method) ,$args);
+}
+
+function post_cart_livraison() {
+	$args = func_get_args();
+	$api = array_shift($args);
+	$method = array_shift($args);
+
+	$livraison = new API_Livraison($api);
+	return call_user_func_array(array($livraison, $method) ,$args);
+}
