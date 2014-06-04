d07e0f1c5645dab97d8b9bf21c3b63b3dae79758
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2014-06-04 15:51:56 +0000

    Réorganisation de la page de gestion des billets de blog Système d'onglets comme pour les produits
    git-svn-id: svn://svn.noparking.net/usr/local/svn/alticcio@3113 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- admin/pages/010_contenu.blogpost.php
+++ admin/pages/010_contenu.blogpost.php
@@ -22,11 +22,14 @@ $page->javascript[] = $config->core_media("jquery.min.js");
 $page->javascript[] = $config->core_media("jquery.form.js");
 $page->javascript[] = $config->core_media("jquery.dteditor.js");
 $page->javascript[] = $config->media("blog.js");
+$page->javascript[] = $config->media("produit.js");
 $page->javascript[] = $url->make("DTEditor");
 $page->css[] = $config->media("dteditor.css");
 $page->jsvars[] = array(
 	"edit_url" => $url2->make("current", array('action' => 'edit', 'id' => "")),	
 );
+
+$page->css[] = $config->media("produit.css");
 
 /*
  * Variables de navigation
@@ -86,6 +89,12 @@ TEMPLATE;
 
 $message = "";
 
+$section = "contenu";
+if ($form->value('section')) {
+	$section = $form->value('section');
+}
+$traduction = $form->value("lang");
+
 /*
  * Traitement du formulaire
  */
@@ -140,30 +149,50 @@ else {
  * Rendu HTML
  */
 
-$buttons['new'] = $page->l($dico->t("Nouveau"), $url2->make("current", array('action' => "create")));
-$buttons['list'] = $page->l($dico->t("VoirListe"), $url2->make("current", array('action' => "list")));
+if ($action == "create") {
+	$titre_page =  $dico->t("CreerBillet");
+}
+else if ($action == "edit") {
+	$blog_id = $url2->get('id');
+	$blogpost->load($blog_id);
+	$titre_page =  $dico->t("EditerBillet");
+	$form->default_values['blogpost'] = $blogpost->values;
+	$form->default_values['themes'] = $blogpost->themes;
+}
+$buttons['new'] = $page->l($dico->t("Nouveau"), $url2->make("current", array('action' => "create", 'id' => "")));
 
 $blogs = $user->blogblogs();
 $id_blog = isset($data['id_blog']) ? $data['id_blog'] : key($blogs);
 
-switch ($url2->get('action', true)) {
-	case "create" :
-		$titre_page =  $dico->t("CreerBillet");
-		$form_start = $form->form_start();
-		$main = <<<HTML
-		{$message}
-{$form->fieldset_start($dico->t('VotreBillet:'))}
+if ($action == "create" or $action == "edit") {
+	$buttons[] = $form->input(array('name' => "save", 'type' => "submit", 'value' => $dico->t("Enregistrer"), 'template' => "#{field}"));
+	$buttons['list'] = $page->l($dico->t("VoirListe"), $url2->make("current", array('action' => "list", 'id' => "")));
+	$sections = array(
+		'contenu' => $dico->t('Contenu'),
+		'referencement' => $dico->t('Referencement'),
+		'themes' => $dico->t('Themes'),
+		'produits' => $dico->t('Produits'),
+	);
+
+	// variable $hidden mise à jour dans ce snippet
+	$left = $page->inc("snippets/produits-sections");
+
+	$form_start = $form->form_start();
+	$main = <<<HTML
+{$message}
+{$form->input(array('type' => "hidden", 'name' => "section", 'value' => $section))}
+{$form->fieldset_start(array('legend' => $dico->t('VotreBillet:'), 'class' => "produit-section produit-section-contenu".$hidden['contenu'], 'id' => "produit-section-contenu"))}
 {$form->input(array('name' => "blogpost[titre]", 'id' => 'titre', 'label' => $dico->t("Titre")))}
 {$form->textarea(array('name' => "blogpost[texte]", 'id' => 'texte', 'class' => "dteditor", 'label' => $dico->t("Texte")))}
+{$form->select(array('name' => "blogpost[id_langues]", 'id' => "id_langues", 'label' => $dico->t("Langue"), 'options' => $user->bloglangues(), 'template' => $template_date))}
+{$form->input(array('type' => "checkbox", 'name' => "blogpost[affichage]", 'id' => 'checkbox', 'value' => 1, 'label' => $dico->t("Affichage"), 'template' => $template_checkbox))}
+{$form->date(array('name' => "blogpost[date_affichage]", 'id' => 'date_affichage', 'label' => $dico->t("DateAffichage"), 'format' => $dico->d("FormatDate") ))}
 {$form->fieldset_end()}
-{$form->fieldset_start($dico->t('Referencement:'))}
+{$form->fieldset_start(array('legend' => $dico->t('Referencement:'), 'class' => "produit-section produit-section-referencement".$hidden['referencement'], 'id' => "produit-section-referencement"))}
 {$form->input(array('name' => "blogpost[meta_title]", 'id' => 'meta_title', 'label' => $dico->t("MetaTitle")))}
 {$form->textarea(array('name' => "blogpost[meta_description]", 'id' => 'meta_description', 'label' => $dico->t("MetaDescription")))}
 {$form->textarea(array('name' => "blogpost[meta_keywords]", 'id' => 'meta_keywords', 'label' => $dico->t("MetaKeywords")))}
 {$form->input(array('name' => "blogpost[titre_url]", 'id' => 'titre_url', 'label' => $dico->t("TitreURL")))}
-{$form->fieldset_end()}
-{$form->fieldset_start($dico->t('Validation:'))}
-{$form->input(array('name' => "create", 'type' => "submit", 'value' => $dico->t("Creer"), 'template' => "#{field}"))}
 {$form->fieldset_end()}
 HTML;
 	$themes_checkboxes = "<ul>";
@@ -175,85 +204,25 @@ HTML;
 	}
 	$themes_checkboxes .= "</ul>";
 	$themes_options = options_select_tree(DBTools::tree($theme->all_themes($id_blog)), $form, "themes");
-	$right = <<<HTML
-{$form->fieldset_start($dico->t("Langue"))}
-{$form->select(array('name' => "blogpost[id_langues]", 'id' => "id_langues", 'label' => $dico->t("Langue"), 'options' => $user->bloglangues(), 'template' => $template_date))}
-{$form->fieldset_end()}
-{$form->fieldset_start($dico->t('Affichage:'))}
-{$form->input(array('type' => "checkbox", 'name' => "blogpost[affichage]", 'id' => 'checkbox', 'value' => 1, 'label' => $dico->t("Affichage"), 'template' => $template_checkbox))}
-{$form->date(array('name' => "blogpost[date_affichage]", 'id' => 'date_affichage', 'label' => $dico->t("DateAffichage"), 'format' => $dico->d("FormatDate") ))}
-{$form->fieldset_end()}
-{$form->fieldset_start($dico->t("Themes"))}
+	$main .= <<<HTML
+{$form->fieldset_start(array('legend' => $dico->t('Themes'), 'class' => "produit-section produit-section-themes".$hidden['themes'], 'id' => "produit-section-themes"))}
 {$themes_checkboxes}
 {$form->fieldset_end()}
-{$form->fieldset_start($dico->t('NouveauTheme'))}
+{$form->fieldset_start(array('legend' => $dico->t('NouveauTheme'), 'class' => "produit-section produit-section-themes".$hidden['themes'], 'id' => "produit-section-themes"))}
 {$form->input(array('name' => "theme[nom]", 'label' => $dico->t('Nom') ))}
 {$form->select(array('name' => "id_blog", 'label' => $dico->t('Blog'), 'options' => $blogs))}
 {$form->select(array('name' => "theme[id_parent]", 'label' => $dico->t('ThemeParent'), 'options' => $themes_options))}
 {$form->input(array('name' => "add-theme", 'type' => "submit", 'value' => $dico->t('Ajouter'), 'template' => "#{field}"))}
 {$form->fieldset_end()}
 HTML;
-		$form_end = $form->form_end();
-		break;
-	
-	case "edit" :
-		$blog_id = $url2->get('id');
-		$buttons['save'] = $form->input(array('name' => "save", 'type' => "submit", 'value' => $dico->t("Sauvegarder"), 'template' => "#{field}"));
-		$buttons['delete'] = $form->input(array('name' => "delete", 'type' => "submit", 'value' => $dico->t("Supprimer"), 'template' => "#{field}"));
-		$buttons['comments'] = $page->l($dico->t('Commentaires'), $url4->make("current", array('type' => "comments", 'action' => "list", 'nature' => "blog", "id" => $blog_id)));
-		$blogpost->load($blog_id);
-		$titre_page =  $dico->t("EditerBillet");
-		$form->default_values['blogpost'] = $blogpost->values;
-		$form->default_values['themes'] = $blogpost->themes;
-		$form->check();
-		$form_start = $form->form_start();
-		$main = <<<HTML
-		{$message}
-{$form->fieldset_start($dico->t('VotreBillet:'))}
-{$form->input(array('type' => "hidden", 'name' => "blogpost[id]"))}
-{$form->input(array('name' => "blogpost[titre]", 'id' => 'titre', 'label' => $dico->t("Titre")))}
-{$form->textarea(array('name' => "blogpost[texte]", 'id' => 'dteditor', 'class' => "dteditor", 'label' => $dico->t("Texte")))}
-{$form->fieldset_end()}
-{$form->fieldset_start($dico->t('Referencement:'))}
-{$form->input(array('name' => "blogpost[meta_title]", 'id' => 'meta_title', 'label' => $dico->t("MetaTitle")))}
-{$form->textarea(array('name' => "blogpost[meta_description]", 'id' => 'meta_description', 'label' => $dico->t("MetaDescription")))}
-{$form->textarea(array('name' => "blogpost[meta_keywords]", 'id' => 'meta_keywords', 'label' => $dico->t("MetaKeywords")))}
-{$form->input(array('name' => "blogpost[titre_url]", 'id' => 'titre_url', 'label' => $dico->t("TitreURL")))}
-{$form->fieldset_end()}
-HTML;
-	$themes_checkboxes = "<ul>";
-	foreach ($blogs as $idblog => $blog) {
-		$themes_checkboxes .= "<li>";
-		$themes_checkboxes .= "<h3>$blog</h3>";
-		$themes_checkboxes .= print_checkbox_tree(DBTools::tree($theme->all_themes($idblog)), $form, array(), "themes") ;
-		$themes_checkboxes .= "</li>";
+	if ($action == "edit") {
+		$main .= $form->input(array('type' => "hidden", 'name' => "blogpost[id]"));
 	}
-	$themes_checkboxes .= "</ul>";
-	$themes_options = options_select_tree(DBTools::tree($theme->all_themes($id_blog)), $form, "themes");
-	$right = <<<HTML
-{$form->fieldset_start($dico->t("Langue"))}
-{$form->select(array('name' => "blogpost[id_langues]", 'id' => "id_langues", 'label' => $dico->t("Langue"), 'options' => $user->bloglangues(), 'template' => $template_date))}
-{$form->fieldset_end()}
-{$form->fieldset_start($dico->t('Affichage:'))}
-{$form->input(array('type' => "checkbox", 'name' => "blogpost[affichage]", 'id' => 'checkbox', 'value' => 1, 'label' => $dico->t("Affichage"), 'template' => $template_checkbox))}
-{$form->date(array('name' => "blogpost[date_affichage]", 'id' => 'date_affichage', 'label' => $dico->t("DateAffichage"), 'format' => $dico->d("FormatDate")))}
-{$form->fieldset_end()}
-{$form->fieldset_start($dico->t("Themes"))}
-{$themes_checkboxes}
-{$form->fieldset_end()}
-{$form->fieldset_start($dico->t('NouveauTheme'))}
-{$form->input(array('name' => "theme[nom]", 'label' => $dico->t('Nom')))}
-{$form->select(array('name' => "id_blog", 'label' => $dico->t('Blog'), 'options' => $blogs))}
-{$form->select(array('name' => "theme[id_parent]", 'label' => $dico->t('ThemeParent'), 'options' => $themes_options))}
-{$form->input(array('name' => "add-theme", 'type' => "submit", 'value' => $dico->t('Ajouter'), 'template' => "#{field}"))}
-{$form->fieldset_end()}
-HTML;
-		$form_end = $form->form_end();
-		break;
-	
-	default :
-		$titre_page =  $dico->t("GestionBillets");
-		$q = <<<SQL
+	$form_end = $form->form_end();
+}
+else {
+	$titre_page =  $dico->t("GestionBillets");
+	$q = <<<SQL
 SELECT DISTINCT(b.id), b.titre, u.login, lg.code_langue, b.affichage, b.date_affichage 
 FROM dt_billets AS b
 LEFT OUTER JOIN dt_billets_themes_blogs AS btb ON btb.id_billets = b.id
@@ -261,58 +230,53 @@ LEFT OUTER JOIN dt_langues AS lg ON lg.id = b.id_langues
 LEFT OUTER JOIN dt_themes_blogs AS tb ON tb.id = btb.id_themes_blogs
 LEFT OUTER JOIN dt_users AS u ON u.id = b.id_users
 SQL;
-		if ($user_data['id_groupes_users'] != 99) {
-			$q .= "\n".<<<SQL
+	if ($user_data['id_groupes_users'] != 99) {
+		$q .= "\n".<<<SQL
 LEFT OUTER JOIN dt_blogs_themes_blogs AS bltb ON bltb.id_themes_blogs = tb.id 
 LEFT OUTER JOIN dt_users_blogs AS ub ON ub.id_blogs = bltb.id_blogs
 WHERE ub.id_users = {$user_data['id']} OR b.id_users = {$user_data['id']}
 SQL;
-		}
-		$pager = new Pager($sql, array(20, 30, 50, 100, 200));
-		$filter = new Filter($pager, array(
-			'id' => array(
-				'field' => 'b.id',
-				'title' => 'ID',
-				'type' => 'between',
-				'order' => 'DESC',
-			),
-			'titre' => array(
-				'field' => 'b.titre',
-				'title' => $dico->t('Titre'),
-				'type' => 'contain',
-			),
-			'login' => array(
-				'field' => 'u.login',
-				'title' => $dico->t('Auteur'),
-				'type' => 'contain',
-			),
-			'code_langue' => array(
-				'field' => 'lg.code_langue',
-				'title' => $dico->t('Langue'),
-				'type' => 'equal',
-			),
-			'affichage' => array(
-				'field' => 'b.affichage',
-				'title' => $dico->t('Affichage'),
-			),
-			'date_affichage' => array(
-				'field' => 'b.date_affichage',
-				'title' => $dico->t('Date'),
-				'type' => "date",
-			)
-		), array(), "filter_blogpost");
-		$res = $filter->query($q);
-		$filter->fetchall($res);
-		$main = <<<HTML
+	}
+	$pager = new Pager($sql, array(20, 30, 50, 100, 200));
+	$filter = new Filter($pager, array(
+		'id' => array(
+			'field' => 'b.id',
+			'title' => 'ID',
+			'type' => 'between',
+			'order' => 'DESC',
+		),
+		'titre' => array(
+			'field' => 'b.titre',
+			'title' => $dico->t('Titre'),
+			'type' => 'contain',
+		),
+		'login' => array(
+			'field' => 'u.login',
+			'title' => $dico->t('Auteur'),
+			'type' => 'contain',
+		),
+		'code_langue' => array(
+			'field' => 'lg.code_langue',
+			'title' => $dico->t('Langue'),
+			'type' => 'equal',
+		),
+		'affichage' => array(
+			'field' => 'b.affichage',
+			'title' => $dico->t('Affichage'),
+		),
+		'date_affichage' => array(
+			'field' => 'b.date_affichage',
+			'title' => $dico->t('Date'),
+			'type' => "date",
+		)
+	), array(), "filter_blogpost");
+	$res = $filter->query($q);
+	$filter->fetchall($res);
+	$main = <<<HTML
 <form action="" method="post">
 {$page->inc("snippets/filter")}
 </form>
 HTML;
-		break;
-
-	case "comments" :
-		$titre_page = $dico->t('CommentairesDuBillet')." #".$id;
-		break;
 }
 
 foreach ($form->errors() as $error) { 
