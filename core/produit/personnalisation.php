7d6c737498ada4eb7862a7605ba445ba6ea753f6
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2014-09-02 09:36:38 +0000

    Objet pour gérer la personnalisation. Génération du HTML éditable d'un produit personnalisable
    git-svn-id: svn://svn.noparking.net/usr/local/svn/alticcio@3178 4f6a263e-5ccd-11de-98f9-81b68c04c31c

---
+++ core/produit/personnalisation.php
@@ -0,0 +1,115 @@
+<?php
+
+require_once "abstract_object.php";
+
+class Personnalisation {
+
+	function __construct($sql, $url, $path_files, $path_www) {
+		$this->sql = $sql;
+		$this->url = $url;
+		$this->path_files = $path_files;
+		$this->path_www = $path_www;
+	}
+
+	function get_default($id_produits) {
+		$personnalisations = array(
+			'textes' => array(),
+			'images' => array(),
+		);
+		$q = <<<SQL
+SELECT * FROM dt_produits_perso_textes WHERE id_produits = {$id_produits}
+SQL;
+		$res = $this->sql->query($q);
+		while ($row = $this->sql->fetch($res)) {
+			$personnalisations['textes'][$row['id']] = $row;
+		}
+
+		$q = <<<SQL
+SELECT * FROM dt_produits_perso_images WHERE id_produits = {$id_produits}
+SQL;
+		$res = $this->sql->query($q);
+		while ($row = $this->sql->fetch($res)) {
+			$personnalisations['images'][$row['id']] = $row;
+		}
+
+		return $personnalisations;
+	}
+
+	function edit_default($id_produits) {
+		$html = <<<HTML
+<div class="personnalisation-produit" id="personnalisation-produit-{$id_produits}" style="text-align: center;">
+<div class="personnalisation-produit-element" style="display: inline-block; position: relative;">
+HTML;
+		$personnalisations = $this->get_default($id_produits);
+		foreach($personnalisations['textes'] as $id_texte => $texte) {
+			$css = "";
+			$css .= <<<CSS
+position: absolute;
+z-index: 1;
+resize: none;
+overflow: hidden;
+CSS;
+			$css .= $texte['css'];
+			$css = preg_replace("/\s+/", " ", $css);
+			$readonly = $texte['locked'] ? "readonly" : "";
+			$html .= <<<HTML
+<textarea {$readonly} class="personnalisation-produit-texte" style="{$css}">{$texte['contenu']}</textarea>
+HTML;
+		}
+		foreach($personnalisations['images'] as $id_image => $image) {
+			$css = "";
+			if ($image['background']) {
+				$css .= "position: relative; z-index: 0;";
+			}
+			else {
+				$css .= "position: absolute; z-index: 1;";
+			}
+			$css .= <<<CSS
+background-image: url({$this->url}{$image['fichier']});
+background-size: contain;
+background-position: center;
+background-repeat: no-repeat;
+CSS;
+			$css .= $image['css'];
+			$css = preg_replace("/\s+/", " ", $css);
+			$input = "";
+			$editable = "";
+			if (!$image['locked']) {
+				$input = <<<HTML
+<table style="height: 100%; width: 100%;"><tr><td style="vertical-align: middle;">
+<input type="file" style="display: none;" />
+</td></tr></table>
+HTML;
+				$editable = "editable";
+			}
+			$html .= <<<HTML
+<div class="personnalisation-produit-image {$editable}" style="{$css}">{$input}</div>
+HTML;
+		}
+		$html .= <<<HTML
+</div>
+</div>
+HTML;
+
+		return $html;
+	}
+
+	function add_image($fichier) {
+		if ($name = $fichier['name']) {
+			$tmp_name = $fichier['tmp_name'];
+			preg_match("/(\.[^\.]*)$/", $name, $matches);
+			$ext = $matches[1];
+			$file_name = md5_file($tmp_name).$ext;
+			move_uploaded_file($tmp_name, $this->path_files.$file_name);
+			copy($this->path_files.$file_name, $this->path_www.$file_name);
+
+			return array(
+				'url' => $this->url.$file_name,
+			);
+		}
+
+		return array(
+			'url' => "",
+		);
+	}
+}
