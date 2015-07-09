38c35a0d44f8bf4bc35a05dcbcc7ea4c404f327d
jeff <jeff@4f6a263e-5ccd-11de-98f9-81b68c04c31c>
2015-07-09 16:00:29 +0000

    Nouvelle correction dans l'update
    git-svn-id: svn://svn.noparking.net/usr/local/svn/alticcio@3349 4f6a263e-5ccd-11de-98f9-81b68c04c31c

--- admin/includes/update.inc.php
+++ admin/includes/update.inc.php
@@ -1397,7 +1397,7 @@ SQL;
 
 function update_53($update) {
 	$q = <<<SQL
-ALTER TABLE `dt_commandes_produits` ADD `franco` TINYINT( 1 ) NOT NULL DEFAULT '0' AFTER `echantillon`, ADD `frais_port` FLOAT NOT NULL DEFAULT '0' AFTER `franco`
+ALTER TABLE `dt_prix` ADD `frais_port` FLOAT NOT NULL DEFAULT '0' AFTER `franco`
 SQL;
 	$update->query($q);
 
